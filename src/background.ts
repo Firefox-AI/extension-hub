3b4df8260785bf35dc518353aae91b80436bf131
Nick Grato <ngrato@gmail.com>
2025-07-25 11:53:32 -0700

    adding an abstraction layer for page assiting aka page buddy

--- src/background.ts
+++ src/background.ts
@@ -2,10 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+import { getPlanResponse } from './services/plan-checklist'
 import { MessageTypesT, MessagePageAssistT } from '../types'
 import { getPageAssistResponse } from './services/pageAssist'
 import { getHuggingFaceChatResponse } from './services/huggingface'
-import { getPlanResponse } from './services/plan-checklist'
 import initContextMenus from './contextMenu'
 import { summarizeTabs } from './services/browserHistory'
 import { initEnvironment } from './systemConfig'
