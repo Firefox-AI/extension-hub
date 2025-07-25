840c038e48f0aa1fc2244413ca6c3b02cc05f3ad
Nick Grato <ngrato@gmail.com>
2025-07-25 12:10:09 -0700

    updating rebse?

--- src/background.ts
+++ src/background.ts
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import { getPlanResponse } from './services/plan-checklist'
 import { MessageTypesT, MessagePageAssistT } from '../types'
 import { getPageAssistResponse } from './services/pageAssist'
 import { getHuggingFaceChatResponse } from './services/huggingface'
